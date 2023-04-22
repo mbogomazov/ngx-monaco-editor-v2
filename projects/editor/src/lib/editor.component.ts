e9880be23d4dfc7507f15b97f8bfaf28761c05fc
Mikhail Bogomazov <ihackit595@gmail.com>
2023-04-22 20:15:44 +0300

    add draft version of emmet integration

--- projects/editor/src/lib/editor.component.ts
+++ projects/editor/src/lib/editor.component.ts
@@ -1,6 +1,9 @@
 import { Component, forwardRef, Inject, Input, NgZone } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { fromEvent } from 'rxjs';
+import * as Monaco from 'monaco-editor';
+
+import { emmetHTML, emmetCSS, emmetJSX, Dispose } from 'emmet-monaco-es';
 
 import { BaseEditor } from './base-editor';
 import { NGX_MONACO_EDITOR_CONFIG, NgxMonacoEditorConfig } from './config';
@@ -11,24 +14,31 @@ declare var monaco: any;
 @Component({
   selector: 'ngx-monaco-editor',
   template: '<div class="editor-container" #editorContainer></div>',
-  styles: [`
+  styles: [
+    `
       :host {
-          display: block;
-          height: 200px;
+        display: block;
+        height: 200px;
       }
 
       .editor-container {
-          width: 100%;
-          height: 98%;
+        width: 100%;
+        height: 98%;
       }
-  `],
-  providers: [{
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: forwardRef(() => EditorComponent),
-    multi: true
-  }]
+    `,
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => EditorComponent),
+      multi: true,
+    },
+  ],
 })
-export class EditorComponent extends BaseEditor implements ControlValueAccessor {
+export class EditorComponent
+  extends BaseEditor
+  implements ControlValueAccessor
+{
   private _value: string = '';
 
   propagateChange = (_: any) => {};
@@ -56,12 +66,17 @@ export class EditorComponent extends BaseEditor implements ControlValueAccessor 
     }
   }
 
-  constructor(private zone: NgZone, @Inject(NGX_MONACO_EDITOR_CONFIG) private editorConfig: NgxMonacoEditorConfig) {
+  constructor(
+    private zone: NgZone,
+    @Inject(NGX_MONACO_EDITOR_CONFIG)
+    private editorConfig: NgxMonacoEditorConfig
+  ) {
     super(editorConfig);
   }
 
   writeValue(value: any): void {
     this._value = value || '';
+
     // Fix for value change while dispose in process.
     setTimeout(() => {
       if (this._editor && !this.options.model) {
@@ -70,41 +85,49 @@ export class EditorComponent extends BaseEditor implements ControlValueAccessor 
     });
   }
 
-  registerOnChange(fn: any): void {
+  registerOnChange(fn: () => void): void {
     this.propagateChange = fn;
   }
 
-  registerOnTouched(fn: any): void {
+  registerOnTouched(fn: () => void): void {
     this.onTouched = fn;
   }
 
   protected initMonaco(options: any, insideNg: boolean): void {
-
     const hasModel = !!options.model;
+
+    if (options.useEmmet) {
+      this.initEmmet(options);
+    }
 
     if (hasModel) {
       const model = monaco.editor.getModel(options.model.uri || '');
+
       if (model) {
         options.model = model;
         options.model.setValue(this._value);
       } else {
-        options.model = monaco.editor.createModel(options.model.value, options.model.language, options.model.uri);
+        options.model = monaco.editor.createModel(
+          options.model.value,
+          options.model.language,
+          options.model.uri
+        );
       }
     }
 
     if (insideNg) {
-      this._editor = monaco.editor.create(this._editorContainer.nativeElement, options);
+      this.createMonaco(options);
     } else {
       this.zone.runOutsideAngular(() => {
-        this._editor = monaco.editor.create(this._editorContainer.nativeElement, options);
-      })
+        this.createMonaco(options);
+      });
     }
 
     if (!hasModel) {
       this._editor.setValue(this._value);
     }
 
-    this._editor.onDidChangeModelContent((e: any) => {
+    this._editor.onDidChangeModelContent(() => {
       const value = this._editor.getValue();
 
       // value is not propagated to parent when executing outside zone.
@@ -122,8 +145,31 @@ export class EditorComponent extends BaseEditor implements ControlValueAccessor 
     if (this._windowResizeSubscription) {
       this._windowResizeSubscription.unsubscribe();
     }
-    this._windowResizeSubscription = fromEvent(window, 'resize').subscribe(() => this._editor.layout());
+    this._windowResizeSubscription = fromEvent(window, 'resize').subscribe(() =>
+      this._editor.layout()
+    );
+
     this.onInit.emit(this._editor);
   }
 
+  private createMonaco(options) {
+    this._editor = monaco.editor.create(
+      this._editorContainer.nativeElement,
+      options
+    );
+  }
+
+  private initEmmet(options: any) {
+    const emmetInitializers: Record<
+      string,
+      (monaco?: typeof Monaco, languages?: string[]) => Dispose
+    > = {
+      html: emmetHTML,
+      css: emmetCSS,
+      javascript: emmetJSX,
+    };
+
+    // TODO: implement emmet dispose
+    emmetInitializers[options.language]();
+  }
 }
